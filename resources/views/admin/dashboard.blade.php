cfe044a50dca23ba6ac94de6b81c0e517cfebfd2
senhoang <nguyenhoangsen198@gmail.com>
2021-09-07 02:21:05 +0700

    Login ok

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('admin_layout')
+@section('admin_content')
+<section class="wrapper">
+    <h3>Welcome to admin </h3>
+</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\HomeController;
+use \App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Frontend
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index']);
+
+
+// Backend
+Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/dashboard', [AdminController::class, 'show_dashboard']);
+Route::get('/logout', [AdminController::class, 'logout']);
+Route::post('/admin-dashboard', [AdminController::class, 'dashboard']);
